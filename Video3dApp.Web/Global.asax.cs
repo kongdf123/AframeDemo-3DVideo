c4be0c389afc3e22818fd7650de5fde1197c37aa
Kent <kongdf123@hotmail.com>
2017-07-12 03:16:09 +0800

    Add project files.

---
+++ Video3dApp.Web/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Video3dApp.Web
+{
+	public class MvcApplication : HttpApplication
+	{
+		protected void Application_Start() {
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		public static void RegisterRoutes(RouteCollection routes) {
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Video3d", action = "Detail", id = UrlParameter.Optional }
+			);
+		}
+	}
+}
